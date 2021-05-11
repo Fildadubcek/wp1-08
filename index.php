f98a5708cdb1f879eff3fe4b3ada35eadb1fc7ed
Fildadubcek <dubcekf@gmail.com>
2021-05-11 11:37:38 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<h1>Vyber oblíbené hry</h1>
+<form>
+<input type="checkbox" name="Far Cry 3" id="Far Cry 3" value ="Far Cry 3">
+<label for="Far Cry 3">Far Cry 3</label><br>
+<input type="checkbox" name="Far Cry 4" id="Far cry 4" value="Far Cry 4">
+<label for="Far Cry 4">Far Cry 4</label><br>
+<input type="checkbox" name="Dark Souls 3" id="Dark Souls 3" value="Dark Souls 3">
+<label for="Dark Souls 3">Dark Souls 3</label><br>
+<input type ="checkbox" name="Mario" id="Mario" value="Mario">
+<label for="Mario">Marion </labrl><br>
+<input type="checkbox" name="CSGO" id="CSGO" value="CSGO">
+<label for="CSGO">CSGO</label><br>
+<input type="checkbox" name="Valorant" id="Valorant" value="Valorant">
+<label for="Valorant">Valorant</label><br>
+<input type="checkbox" name="Assassins Creed" id="Assassins Creed" value="Assassins Creed">
+<label for="Assassins Creed">Assassins Creed</label><br>
+<input type="checkbox" name="Need For Speed" id="Need For Speed" value="Need For Speed">
+<label for="Need For Speed">Need For Speed</label><br>
+<input type="checkbox" name="Crew" id="Crew" value="Crew">
+<label for="Crew">Crew</label><br>
+<input type="checkbox" name="Flatout 2" id="Flatout2" value="Flatout 2">
+<label for="Flatout 2">Flatout 2</label><br>
+</form>
+</body>
+</html>
