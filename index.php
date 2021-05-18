c8b032494f6976bd64ad9372b452e01aeede2d22
Fildadubcek <dubcekf@gmail.com>
2021-05-18 11:43:29 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+$submit = filter_input(INPUT_POST, 'submit');
+if(isset($_POST['games'])) {
+ $games = $_POST ['games'];   
+}
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,31 +19,40 @@
     <title>Document</title>
 </head>
 <body>
-    
+ <?php
+ if(isset($submit)){
+     echo "formulář byl odeslán";
+     var_dump($games);
+ }  else  {?>
+ 
+ 
 
 
 <h1>Vyber oblíbené hry</h1>
-<form>
-<input type="checkbox" name="Far Cry 3" id="Far Cry 3" value ="Far Cry 3">
-<label for="Far Cry 3">Far Cry 3</label><br>
-<input type="checkbox" name="Far Cry 4" id="Far cry 4" value="Far Cry 4">
-<label for="Far Cry 4">Far Cry 4</label><br>
-<input type="checkbox" name="Dark Souls 3" id="Dark Souls 3" value="Dark Souls 3">
-<label for="Dark Souls 3">Dark Souls 3</label><br>
-<input type ="checkbox" name="Mario" id="Mario" value="Mario">
+<form action ="index.php" method="post">
+<input type="checkbox" name="games[]" id="Farcry3" value ="Farcry3">
+<label for="Farcry 3">Far Cry 3</label><br>
+<input type="checkbox" name="games[]" id="Farcry 4" value="Farcry4">
+<label for="Farcry4">Far Cry 4</label><br>
+<input type="checkbox" name="games[]" id="Darksouls3" value="Darksouls3">
+<label for="Darksouls3">Dark Souls 3</label><br>
+<input type ="checkbox" name="games[]" id="Mario" value="Mario">
 <label for="Mario">Marion </labrl><br>
-<input type="checkbox" name="CSGO" id="CSGO" value="CSGO">
+<input type="checkbox" name="games[]" id="CSGO" value="CSGO">
 <label for="CSGO">CSGO</label><br>
-<input type="checkbox" name="Valorant" id="Valorant" value="Valorant">
+<input type="checkbox" name="games[]" id="Valorant" value="Valorant">
 <label for="Valorant">Valorant</label><br>
-<input type="checkbox" name="Assassins Creed" id="Assassins Creed" value="Assassins Creed">
-<label for="Assassins Creed">Assassins Creed</label><br>
-<input type="checkbox" name="Need For Speed" id="Need For Speed" value="Need For Speed">
-<label for="Need For Speed">Need For Speed</label><br>
-<input type="checkbox" name="Crew" id="Crew" value="Crew">
+<input type="checkbox" name="games[]" id="Assassinscreed" value="Assassinscreed">
+<label for="Assassinscreed">Assassins Creed</label><br>
+<input type="checkbox" name="games[]" id="Needforspeed" value="Needforspeed">
+<label for="Needforspeed">Need For Speed</label><br>
+<input type="checkbox" name="games[]" id="Crew" value="Crew">
 <label for="Crew">Crew</label><br>
-<input type="checkbox" name="Flatout 2" id="Flatout2" value="Flatout 2">
-<label for="Flatout 2">Flatout 2</label><br>
+<input type="checkbox" name="games[]" id="Flatout2" value="Flatout2">
+<label for="Flatout2">Flatout 2</label><br>
+<input type="submit" name="submit" value="odeslat">
+
 </form>
+ <?php } ?>
 </body>
 </html>
